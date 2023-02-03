9e27c8e5ba190d1d346967e15492608ebf5a8a0c
EufratesLC <122678982+EufratesLC@users.noreply.github.com>
2023-02-03 13:43:05 +0200

    Coloquei o conteudo

---
+++ program1i.asm
@@ -0,0 +1,16 @@
+TITLE NOME: DESCRICAO
+
+.MODEL SMALL
+
+.STACK 100H
+
+.DATA
+;os dados vao aqui
+
+.CODE
+
+.MAIN PROC
+;as instrucoes vem aqui
+MAIN ENDP
+;outros procedimentos vem aqui
+END MAIN
